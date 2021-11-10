d5153086c04a1e63770d667f53f62d5e38c0489f
Roman Igorevich <roman.igorevich.ru@gmail.com>
2021-11-10 11:21:25 +0300

    подключение винограда

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="gjs">
+    <h1>Hello World Component!</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import grapesjs from "grapesjs";
+// import greapejspreset from "grapesjs-preset-webpage";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      editor: null,
+    };
+  },
+
+  mounted() {
+    this.editor = grapesjs.init({
+      // Indicate where to init the editor. You can also pass an HTMLElement
+      container: "#gjs",
+      // Get the content for the canvas directly from the element
+      // As an alternative we could use: `components: '<h1>Hello World Component!</h1>'`,
+      fromElement: true,
+      // Size of the editor
+      height: "1000px",
+      width: "auto",
+      // Disable the storage manager for the moment
+      storageManager: false,
+      // Avoid any default panel
+      panels: { defaults: [] },
+    });
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* Let's highlight canvas boundaries */
+#gjs {
+  border: 3px solid rgb(163, 2, 2);
+}
+
+/* Reset some default styling */
+.gjs-cv-canvas {
+  top: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
